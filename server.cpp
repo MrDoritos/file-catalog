1e30930324b5e7c3f2c305abe2055a4e3a53df98
root <root@localhost.localdomain>
2019-04-06 10:11:45 +0000

    Linux net test

--- server.cpp
+++ server.cpp
@@ -1,7 +1,22 @@
 #include "userCountPacket.h"
 #include "tcplistener.h"
+#include "tcpclient.h"
+#include <iostream>
 
 int main() {
+tcplistener p = tcplistener();
+if (p.sockbind(4100)) {
+	std::cout << "Could not bind socket" << std::endl;
+	exit(1);
+}
+p.socklisten();
 
-return 0; 
+tcpclient cl;
+while (1) {
+p.accept_client(cl);
+std::cout << "New socket" << std::endl;
+cl.cliwrite((void*)"Tu eres feo", 11);
+cl.cliclose();
+}
+return 0;
 }
