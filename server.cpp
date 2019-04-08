0974979c6a739633c216150c85a78ff41d6ccdcb
MrDoritos <ianmoore322@gmail.com>
2019-04-08 07:52:58 -0400

    test

--- server.cpp
+++ server.cpp
@@ -1,8 +1,9 @@
 #include "userCountPacket.h"
 #include "tcplistener.h"
-#include "tcpclient.h"
+#include "tcpsocketclient.h"
 #include <iostream>
 #include "serverConfig.h"
+#include <string>
 
 int main() {
 serverConfig cnfg((char*)"config.txt");
@@ -16,11 +17,15 @@ if (p.sockbind(4100)) {
 }
 p.socklisten();
 
-tcpclient cl;
+tcpsocketclient cl;
 while (1) {
 p.accept_client(cl);
 std::cout << "New socket" << std::endl;
 cl.cliwrite((void*)"Tu eres feo", 11);
+char buf[256];
+cl.cliread(&buf, 4);
+std::string str(buf, 4);
+std::cout << str << std::endl;
 cl.cliclose();
 }
 
